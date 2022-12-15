0610db4ea974f03db08f4b7b49eaabd4099f2808
anishali2 <ansihali1221@gmail.com>
2022-12-16 01:38:52 +0500

    loader removed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import GlobalStyles from '@/pages/GlobalStyles'
-import Loading from '@/components/Loading/Loader'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
       <Component {...pageProps} />
-      <Loading />
     </>
   )
 }
