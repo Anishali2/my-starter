4bc63a2ed73a16db571231c4859c7d2689536f4d
anishali2 <ansihali1221@gmail.com>
2022-12-16 01:37:07 +0500

    some changes in theme

--- src/components/twin/index.ts
+++ src/components/twin/index.ts
@@ -1,6 +1,7 @@
 import tw from 'twin.macro'
 
-export const Text = tw.div`w-1/2   `
-export const HeaderText = tw.div`w-1/2 text-gray-200 text-2xl font-bold`
-export const SubHeaderText = tw.div`w-1/2 text-black  `
-export const LinkText = tw.div`w-1/2 text-blue-700  `
+export const Text = tw.div`w-1/2`
+// create all the text components like h1, h2, h3, p, span, etc with respective styles
+
+// create a varient of the text component
+export const Heading = tw(Text)`text-2xl`
---
+++ src/pages/components/text/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/twin'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <p></p>
+      <Button />
+    </div>
+  )
+}
+
+export default index
